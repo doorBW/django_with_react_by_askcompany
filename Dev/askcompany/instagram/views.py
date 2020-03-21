ada12d0a9cdc23506a19a13c42bb8424939b2347
doorBW <doorbw@outlook.com>
2020-03-21 15:15:34 +0900

    ch04-4

--- Dev/askcompany/instagram/views.py
+++ Dev/askcompany/instagram/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import ListView
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpRequest, Http404
 from .models import Post
 # Create your views here.
 
@@ -20,12 +20,17 @@ post_list = ListView.as_view(model=Post)
 #         'q': q,
 #      })
 
-def post_detail(request, pk):
-    response = HttpResponse()
-    response.write("Hello World")
-    response.write("Hello World")
-    response.write("Hello World")
-    return response
+# def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+#     post = get_object_or_404(Post, pk=pk)
+#     # try: 
+#     #     post = Post.objects.get(pk=pk) # DoesNotExist 예외
+#     # except Post.DoesNotExist:
+#     #     raise Http404
+#     return render(request, 'instagram/post_detail.html',{
+#         'post': post,
+#     })
+
+post_detail = DetailView.as_view(model=Post)
 
 def archives_year(request,year):
     return HttpResponse(f"{year}년 archives")
