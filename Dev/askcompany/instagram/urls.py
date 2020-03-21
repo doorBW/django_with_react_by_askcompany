beb08cd891c7e7b74055dd12295b40a51cf2bcf6
doorBW <doorbw@outlook.com>
2020-03-21 15:39:30 +0900

    ch04-5

--- Dev/askcompany/instagram/urls.py
+++ Dev/askcompany/instagram/urls.py
@@ -16,7 +16,7 @@ app_name = 'instagram' # URL Reverse에서 namespace역할을 하게 됩니다.
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:pk>/', views.post_detail),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
     # re_path(r'(?P<pk>\d+)/$', views.post_detail),
     path('archives/<year:year>/', views.archives_year),
     # re_path(r'archives/(?P<year>20\d{2})/', views.archives_year),
