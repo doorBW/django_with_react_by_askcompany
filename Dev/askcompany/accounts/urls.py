d3b66a7c4f6349fdf2a4ea2601a6cc04e92d6f4a
doorBW <doorbw@outlook.com>
2020-04-04 18:46:54 +0900

    ch7-1

--- Dev/askcompany/accounts/urls.py
+++ Dev/askcompany/accounts/urls.py
@@ -1,6 +1,7 @@
-
+from django.contrib.auth.views import LoginView
+from django.urls import path
 app_name = 'accounts' # URL Reverse에서 namespace역할을 하게 됩니다.
 
 urlpatterns = [
-    
+    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
 ]
