5e5a59693234964dedc13c2ac1d36740920f57d1
doorBW <doorbw@outlook.com>
2020-04-04 20:27:46 +0900

    ch7-2

--- Dev/askcompany/accounts/urls.py
+++ Dev/askcompany/accounts/urls.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
+from . import views
+
 app_name = 'accounts' # URL Reverse에서 namespace역할을 하게 됩니다.
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.profile_edit, name='profile_edit'),
 ]
--- Dev/askcompany/accounts/views.py
+++ Dev/askcompany/accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, UpdateView
+from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
+
+# @login_required
+# def profile(request):
+#     return render(request, 'accounts/profile.html')
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile.html'
+
+profile = ProfileView.as_view()
+
+# class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+#     model = Profile
+#     form_class = ProfileForm
+
+# profile_edit = ProfileUpdateView.as_view()
+
+@login_required
+def profile_edit(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = None
+    
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('accounts:profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'accounts/profile_form.html', {
+        'form': form,
+    })
