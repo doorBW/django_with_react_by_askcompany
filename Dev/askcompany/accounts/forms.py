ec42510c45ba7eaceb323d65df78877d1dd245c0
doorBW <doorbw@outlook.com>
2020-04-07 21:02:11 +0900

    ch7-4

--- Dev/askcompany/accounts/forms.py
+++ Dev/askcompany/accounts/forms.py
@@ -1,7 +1,18 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Profile
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['address', 'zipcode']
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(help_text='3 + 3 = ?')
+    
+    def clean_answer(self):
+        answer = self.cleaned_data.get('answer')
+        if answer != 6:
+            raise forms.ValidationError("땡~!")
+        return answer
--- Dev/askcompany/accounts/urls.py
+++ Dev/askcompany/accounts/urls.py
@@ -1,11 +1,17 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
+from .forms import LoginForm
 
 app_name = 'accounts' # URL Reverse에서 namespace역할을 하게 됩니다.
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('login/', LoginView.as_view(
+        # form_class=AuthenticationForm,
+        form_class=LoginForm,
+        template_name='accounts/login_form.html'
+        ), name='login'),
     path('logout/', views.logout, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
