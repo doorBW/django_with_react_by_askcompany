aa03e3b24ba244c0d8e20735e254eef8dab001c1
doorBW <doorbw@outlook.com>
2020-04-19 16:13:15 +0900

    ch8-13

--- Dev/django-with-react-rev2/instagram/views.py
+++ Dev/django-with-react-rev2/instagram/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from django.contrib import messages
@@ -25,8 +26,17 @@ def post_new(request):
         "form": form,
     })
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, "instagram/post_detail.html", {
         "post": post,
+    })
+
+def user_page(request, username):
+    page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
+    post_list = Post.objects.filter(author=page_user)
+    return render(request, "instagram/user_page.html", {
+        "page_user": page_user,
+        "post_list": post_list
     })
