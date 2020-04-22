99014f2ada1842df9f3d47d1db635835fed44112
doorBW <doorbw@outlook.com>
2020-04-22 20:24:53 +0900

    ch8-20

--- Dev/django-with-react-rev2/instagram/models.py
+++ Dev/django-with-react-rev2/instagram/models.py
@@ -13,11 +13,12 @@ class BaseModel(models.Model):
         abstract = True
 
 class Post(BaseModel):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='my_post_set', on_delete=models.CASCADE)
     photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
     caption = models.CharField(max_length=500) # models.TextField()
     tag_set = models.ManyToManyField('Tag', blank=True)
     location = models.CharField(max_length=100)
+    like_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='like_post_set') # 아래 LikeUser와 같이 별도 테이블을 만들어서 구현할 수도 있음.
 
     def __str__(self):
         return self.caption
@@ -34,10 +35,18 @@ class Post(BaseModel):
         # return reverse("instagram:post_detail", kwargs={"pk": self.pk})
         return reverse("instagram:post_detail", args=[self.pk])
 
+    def is_like_user(self, user):
+        return self.like_user_set.filter(pk=user.pk).exists()
+
+    class Meta:
+        ordering = ['-id']
+
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
         
-
+# class LikeUser(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
